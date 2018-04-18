ce8f6fd2c995b7629e507480afc4746e915e4820
戎青松 <rongqs1@lenovo.com>
2018-04-18 10:48:11 +0800

    新增验证工具集

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let stringTool=require("./lib/string.lib")//字符串相关工具集
+let vaildTool=require("./lib/vaild.lib")//验证工具集
 module.exports={
-    string:stringTool
+    string:stringTool,
+    vaild:vaildTool
 }
 
 
